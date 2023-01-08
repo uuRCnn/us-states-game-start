dc64d524e3efa69e276f6c5f3b23553a65704443
Ugurcan <ugurcan380@gmail.com>
2023-01-09 00:01:55 +0300

    upgread 1

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ while len(guessed_states) < 50:
 
     answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name").title()
 
+    if answer_state == "Exit".title():
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
     if answer_state in all_states:
         t = turtle.Turtle()
         t.hideturtle()
@@ -23,4 +32,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
 
-screen.exitonclick()
